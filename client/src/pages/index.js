d5af08fad8135b2353944edd313616abdf21ca89
dani <s.leslie94@gmail.com>
2025-06-09 21:21:21 -0400

    update filename

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,5 @@
 // src/pages/index.js
-export { default as HomePage } from "./Homepage";
+export { default as HomePage } from "./HomePage";
 export { default as LoginPage } from "./LoginPage";
 export { default as ProfilePage } from "./ProfilePage";
 export { default as SettingsPage } from "./SettingsPage";
